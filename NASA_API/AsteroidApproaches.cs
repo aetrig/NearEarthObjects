cce002f59a5cfaab2887ff554bebcc56f937ab91
aetrig <emilia.gosztyla@gmail.com>
2025-04-02 18:52:10 +0200

    ...

--- NASA_API/AsteroidApproaches.cs
+++ NASA_API/AsteroidApproaches.cs
@@ -5,13 +5,24 @@ namespace NASA_API;
 internal class AsteroidApproaches : DbContext
 {
 	public DbSet<AsteroidDB> asteroids { get; set; }
-	public DbSet<ApproachesDB> approaches { get; set; }
+	//public DbSet<ApproachesDB> approaches { get; set; }
 	public AsteroidApproaches()
 	{
 		Database.EnsureCreated();
 	}
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlite(@"Data Source=../../../AsteroidApproaches.db");
+        optionsBuilder.UseSqlite(@"Data Source=AsteroidApproaches.db");
     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Diameter>().HasNoKey();
+		modelBuilder.Ignore<Diameter>();
+
+		modelBuilder.Entity<Distance>().HasNoKey();
+		modelBuilder.Ignore<Distance>();
+
+		modelBuilder.Entity<Velocity>().HasNoKey();
+		modelBuilder.Ignore<Velocity>();
+	}
 }
